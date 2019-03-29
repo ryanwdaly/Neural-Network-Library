72c11f6edc7eaaada7a44a827e393cb1509cfd63
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-03-29 01:55:36 -0400

    matrix math sorta done

--- matrix.js
+++ matrix.js
@@ -14,12 +14,25 @@ class Matrix {
     }
     multiply(n) {
         if (n instanceof Matrix) {
-            for(let i = 0; i < this.rows; i++){
-                for(let j = 0; j < this.cols; j++) {
-                    this.matrix[i][j] *= n.matrix[i][j];
-                }
+            if (this.cols !== n.rows) {
+                console.log("Cols of A must match rows of B");
+                return undefined;
             }
+            let a = this;
+            let b = n;
+            let result = new Matrix(a.rows, b.cols);
+            for (let i = 0; i < result.rows; i++) {
+                for (let j = 0; j < result.cols; j++) {
+                    let sum = 0;
+                    for (let k = 0; k < a.cols; k++) {
+                        sum += a.matrix[i][k] * b.matrix[k][j]
+                    }
+                    result.matrix[i][j] = sum;
+                }
+            } 
+            return result;
         } else {
+            // Scallor Product
             for(let i = 0; i < this.rows; i++){
                 for(let j = 0; j < this.cols; j++) {
                     this.matrix[i][j] *= n;
@@ -29,14 +42,18 @@ class Matrix {
     }
     add(n) {
         if (n instanceof Matrix) {
-            for(let i = 0; i < this.rows; i++){
+            if (this.cols != n.rows) {
+                console.log("Cols of A must match rows of B");
+                return undefined;
+            }
+            for (let i = 0; i < this.rows; i++){
                 for(let j = 0; j < this.cols; j++) {
                     this.matrix[i][j] += n.matrix[i][j];
                 }
             }
         } else {
-            for(let i = 0; i < this.rows; i++){
-                for(let j = 0; j < this.cols; j++) {
+            for (let i = 0; i < this.rows; i++){
+                for (let j = 0; j < this.cols; j++) {
                     this.matrix[i][j] += n;
                 }
             }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 var brain;
 
 function setup() {
-    brain = new NeuralNetwork(3, 3, 1)
+    let a = new Matrix(2,3);
+    let b = new Matrix(3, 2);
+
+    a.randomize();
+    b.randomize();
+
+    console.table(a.matrix);
+    console.table(b.matrix);
+
+    let c = a.multiply(b)
+    console.table(c.matrix)
 }
 
 function draw() {
